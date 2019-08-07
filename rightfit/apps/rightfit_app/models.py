00801e1d7135cb4957ea79dfb0d089d67b70ac17
mpatrick1024 <mpatrick1024@gmail.com>
2019-08-07 10:24:36 -0500

    First commit

---
+++ rightfit/apps/rightfit_app/models.py
@@ -0,0 +1,62 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+class UserManager (models.Manager):
+    def register_validator(self, postData):
+        errors = {}
+        email_match = User.objects.filter(email = postData["email"])
+        if len(postData['email'])==0:
+            errors["email_blank"] = "Email can not be blank! Please enter email!"
+        elif len(email_match) > 0:
+            errors["email_invaild"] = "Email exists in database already"
+        
+        if len(postData['first_name'])==0:
+            errors["first_name_blank"] = "First name can not be blank! Please enter a first name!"
+        elif len(postData['first_name']) < 2:
+            errors["first_name"] = "Fist name should be at least 2 characters"
+        if len(postData['last_name'])==0:
+            errors["last_name_blank"] = "First name can not be blank! Please enter a first name!"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "Last name should be at least 2 characters"    
+        if len(postData['password']) < 8:
+            errors["password"] = "Password should be at least 8 characters"
+        if postData['password'] != postData['confirm_password']:
+            errors["password_match"] = "Passwords do not match"
+        return errors
+    
+    def login_validator(self, postData):
+        login_errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        user_to_login= User.objects.get(email=postData["email"])
+        
+        if user_to_login.password != postData['login_password']:
+            login_errors["invalid"] = "Invaild login credentials"
+        else:
+            print("password accepted, logging in ...")
+        return login_errors
+    
+    class User(models.Model):
+        first_name = models.CharField(max_length = 45)
+        last_name = models.CharField(max_length = 45)
+        email = models.EmailField(max_length = 254)
+        password = models.CharField(max_length = 45)    
+        gpa = models.DecimalField()
+        test_score = models.IntegerField()
+        activities = models.CharField( max_length = 254)
+        education_interest = models.CharField( max_length = 254)
+        school_int =  models.CharField( max_length = 254)
+        created_at = models.DateTimeField(auto_now_add = True)
+        updated_at = models.DateTimeField(auto_now = True)
+        objects = UserManager()
+        
+    class School(models.Model):
+        name = models.CharField(max_length = 45) 
+        enrollment = models.CharField(max_length = 254)
+        sports = models.CharField(max_length = 254)
+        avg_test_score = models.IntegerField()
+        gpa = models.DecimalField()
+        created_at = models.DateTimeField(auto_now_add = True)
+        updated_at = models.DateTimeField(auto_now = True)
+        
+    class Activity(models.Model):
---
+++ rightfit/apps/rightfit_app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+from .models import *
+# Create your views here.
+def index(request):
+    return render (request, 'rightfit_app/index.html')
+
+def register (request):
+    errors = User.objects.register_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags='errors')
+        # redirect the user back to the form to fix the errors
+        return redirect('/')
+        
+    else:
+        User.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"], email=request.POST["email"], password=request.POST["password"])
+        last_user = User.objects.last()
+        request.session["user_id"]= last_user.id
+    return redirect('/success')
+
+def login (request):
+    login_errors = User.objects.login_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(login_errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in login_errors.items():
+            messages.error(request, value, extra_tags='login_errors')
+        # redirect the user back to the form to fix the errors
+        return redirect('/')
+    else:
+        user = User.objects.get(email=request.POST["email"])
+        request.session["user_id"]= user.id
+        return redirect('/success')
+    
+    def edit_account(request):
+        user = User.objects.get(id=request.session["user_id"])
+    context = {
+        "user" : user
+    }
+    return render(request, 'rightfit_app/edit_account.html', context)
+
+def account_edit_form(request):
+        user_update = User.objects.get(id=request.session["user_id"])
+        user_update.first_name= request.POST["first_name"]
+        user_update.last_name= request.POST["last_name"]
+        user_update.email= request.POST["email"]
+        user_update.activities= request.POST["activities"]
+        user_update.education_interest= request.POST["education_interest"]
+        user_update.school_int= request.POST["school_int"]
+        user_update.save()
+        return redirect('/edit_account')
