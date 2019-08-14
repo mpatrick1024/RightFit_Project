15e4ad5c9a9a474ac4c3d719910019ec46446437
sunshyneDaCoder <genesisnp528@gmail.com>
2019-08-14 15:18:59 -0500

    new work done

--- rightfit/apps/rightfit_app/urls.py
+++ rightfit/apps/rightfit_app/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^register$', views.register),
     url(r'^login$', views.login),
+    url(r'^register_form$', views.register_form),
+    url(r'^login_form$', views.login_form),
     url(r'^profile$', views.profile),
     url(r'^accountpage$', views.accountpage),
     url(r'^account_edit_form$', views.account_edit_form),
     url(r'^loading$', views.loading),
     url(r'^schoolchoices$',views.schoolchoices),
-    
+    url(r'^schoolmatches$',views.schoolmatches), 
 ]
